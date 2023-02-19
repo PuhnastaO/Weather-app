6dbdd590970090111e84a25af345f436876163c3
PuhnastaO <elena4385@msn.com>
2023-02-19 13:41:27 +0100

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -40,9 +40,21 @@ dateElement.innerHTML = formatDate(response.data.dt*1000);
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-
-
+function search(city) {
 let apiKey = "4aa410cf6a213ee686d681951c8186fb";
-let city = "New York";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
 axios.get(apiUrl).then(displayTemperature);
+
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+search(cityInputElement.value);
+
+}
+
+search("New York");
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
